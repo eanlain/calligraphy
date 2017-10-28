2b2ea7ffa9f0446f440fe2f932a200059e9ebfe7
Brandon Robins <brandon@onebnottwo.com>
2017-10-28 11:59:58 -0500

    Add :allowed_methods and :lock_timeout_period mattr_accessors

--- lib/calligraphy.rb
+++ lib/calligraphy.rb
@@ -16,6 +16,14 @@ module Calligraphy
   DAV_NS = 'DAV:'
   LOCK_TOKEN_REGEX = /<(urn:uuid:.+?)>/
 
+  mattr_accessor :allowed_methods
+  @@allowed_methods = %w(
+    options head get put delete copy move mkcol propfind proppatch lock unlock
+  )
+
+  mattr_accessor :lock_timeout_period
+  @@lock_timeout_period = 24 * 60 * 60
+
   mattr_accessor :web_dav_actions
   @@web_dav_actions = %i(
     options get put delete copy move mkcol propfind proppatch lock unlock
